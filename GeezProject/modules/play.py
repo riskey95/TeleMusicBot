bdc08785d36cd6200020cd1fa3223e117a04d1ae
『TØNIC』 乂 ₭ILLΣR <57848595+widodo01@users.noreply.github.com>
2021-09-03 20:42:11 +0700

    Update play.py

--- GeezProject/modules/play.py
+++ GeezProject/modules/play.py
@@ -615,9 +615,9 @@ async def play(_, message: Message):
             toxxt = "**__silahkan pilih lagu untuk diputar:__**\n\n"
             j = 0
             useer=user_name
-            emojilist = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣"]
+            emojilist = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣",]
 
-            while j < 6:
+            while j < 5:
                 toxxt += f"{emojilist[j]} [{results[j]['title'][:25]}](https://youtube.com{results[j]['url_suffix']})\n"
                 toxxt += f" ├ 💡 **Duration** - {results[j]['duration']}\n"
                 toxxt += f" └ ⚡ __Powered by {BOT_NAME} A.I__\n\n"
@@ -633,9 +633,6 @@ async def play(_, message: Message):
                     [
                         InlineKeyboardButton("4️⃣", callback_data=f'plll 3|{query}|{user_id}'),
                         InlineKeyboardButton("5️⃣", callback_data=f'plll 4|{query}|{user_id}'),
-                    ],
-                    [
-                        InlineKeyboardButton("6️⃣", callback_data=f'plll 5|{query}|{user_id}'),
                     ],
                     [InlineKeyboardButton(text="🗑 Close", callback_data="cls")],
                 ]
